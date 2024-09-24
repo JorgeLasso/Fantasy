995b9ccf7becdfb1bbf19f760ff479ff06df6b43
Jorge Mario Lasso Ortiz <jorgelasso_1992@hotmail.com>
2024-09-24 15:31:49 -0500

    Adding security on users login frontend

--- Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -2,6 +2,7 @@ using Fantasy.Frontend.Repositories;
 using Fantasy.Frontend.Shared;
 using Fantasy.Shared.Entities;
 using Fantasy.Shared.Resources;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using MudBlazor;
@@ -9,6 +10,7 @@ using System.Net;
 
 namespace Fantasy.Frontend.Pages.Countries;
 
+[Authorize(Roles = "Admin")]
 public partial class CountriesIndex
 {
     private List<Country>? Countries { get; set; }
