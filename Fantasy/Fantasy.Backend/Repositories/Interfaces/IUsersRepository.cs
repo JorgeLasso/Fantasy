62f28cfac0d3ddc9920042f9d514a3ad004a7b87
Jorge Mario Lasso Ortiz <jorgelasso_1992@hotmail.com>
2024-09-30 15:50:55 -0500

    Confirm User with Email

--- Fantasy/Fantasy.Backend/Repositories/Interfaces/IUsersRepository.cs
+++ Fantasy/Fantasy.Backend/Repositories/Interfaces/IUsersRepository.cs
@@ -6,6 +6,12 @@ namespace Fantasy.Backend.Repositories.Interfaces;
 
 public interface IUsersRepository
 {
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+    Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
     Task<User> GetUserAsync(string email);
 
     Task<IdentityResult> AddUserAsync(User user, string password);
--- Fantasy/Fantasy.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
+++ Fantasy/Fantasy.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
@@ -6,6 +6,12 @@ namespace Fantasy.Backend.UnitsOfWork.Interfaces;
 
 public interface IUsersUnitOfWork
 {
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+    Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
     Task<User> GetUserAsync(string email);
 
     Task<IdentityResult> AddUserAsync(User user, string password);
